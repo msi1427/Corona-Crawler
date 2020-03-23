a48b7eb78051787faff0a4f6745ca0c3064ccc48
msi1427 <sabikirbaz@iu-dhaka.edu>
2020-03-24 00:01:33 +0600

    headlines,data,article,label,link stored in csv

---
+++ crawler/crawler/spiders/corona_spider.py
@@ -0,0 +1,57 @@
+import scrapy
+from ..items import CrawlerItem
+from bs4 import BeautifulSoup
+
+class QuoteSpider(scrapy.Spider):
+    name = 'corona'
+    start_urls = [
+        'https://www.theguardian.com/au'
+    ]
+    def parse(self, response):
+        items = CrawlerItem()
+        links = response.css("a.js-headline-text::attr(href)").extract()
+        for link in links:
+            yield response.follow(link, callback=lambda r: self.parse_beyond(r, items))
+
+    def parse_beyond(self, response, items):
+        items['link'] = response.url
+        items['headline'] = response.css("h1.content__headline--no-margin-bottom::text").extract()
+        time = response.css("time.content__dateline-wpd")[0].extract()
+        if time is not None:
+            tree = BeautifulSoup(time, "lxml")
+            time = tree.get_text()
+            # print(time)
+            items['time']=time
+        label = response.css("span.label__link-wrapper")[0].extract()
+        if label is not None:
+            tree = BeautifulSoup(label, "lxml")
+            label = tree.get_text()
+            # print(label)
+            items['label']=label
+        byline = response.css("p.byline")[0].extract()
+        if byline is None :
+            byline = response.css("span.content__headline--byline")[0].extract()
+            if byline is not None :
+                tree = BeautifulSoup(byline, "lxml")
+                byline = tree.get_text()
+                # print(byline)
+                items['byline'] = byline
+        else :
+            tree = BeautifulSoup(byline, "lxml")
+            byline = tree.get_text()
+            # print(byline)
+            items['byline'] = byline
+        # print(items)
+        yield items
+
+
+
+
+
+
+
+# HEADLINES
+# DATE AND TIME
+# ARTICLE LINK
+# NEWS LABEL
+# ARTICLE LINK
