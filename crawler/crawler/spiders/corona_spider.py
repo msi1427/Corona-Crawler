f35c772e74042fa1e76752fa6f58e6b63f945311
msi1427 <sabikirbaz@iu-dhaka.edu>
2020-03-24 09:54:40 +0600

    Specify for corona

--- crawler/crawler/spiders/corona_spider.py
+++ crawler/crawler/spiders/corona_spider.py
@@ -14,38 +14,38 @@ class QuoteSpider(scrapy.Spider):
             yield response.follow(link, callback=lambda r: self.parse_beyond(r, items))
 
     def parse_beyond(self, response, items):
-        items['link'] = response.url
+        label = response.css("span.label__link-wrapper")[0].extract()
         headline = response.css("h1.content__headline::text")[0].extract()
         if headline is None:
             headline = response.css("h1.content__headline--no-margin-bottom::text")[0].extract()
-        items['headline']=headline
-        time = response.css("time.content__dateline-wpd")[0].extract()
-        if time is not None:
-            tree = BeautifulSoup(time, "lxml")
-            time = tree.get_text()
-            # print(time)
-            items['time']=time
-        label = response.css("span.label__link-wrapper")[0].extract()
-        if label is not None:
+        if (label.find('Coronavirus') or label.find('coronavirus') or headline.find('Corona') or headline.find('corona') or headline.find('COVID'))  is not None:
             tree = BeautifulSoup(label, "lxml")
             label = tree.get_text()
             # print(label)
-            items['label']=label
-        byline = response.css("p.byline")[0].extract()
-        if byline is None :
-            byline = response.css("span.content__headline--byline")[0].extract()
-            if byline is not None :
+            items['label'] = label
+            items['link'] = response.url
+            items['headline'] = headline
+            time = response.css("time.content__dateline-wpd")[0].extract()
+            if time is not None:
+                tree = BeautifulSoup(time, "lxml")
+                time = tree.get_text()
+                # print(time)
+                items['time'] = time
+            byline = response.css("p.byline")[0].extract()
+            if byline is None :
+                byline = response.css("span.content__headline--byline")[0].extract()
+                if byline is not None :
+                    tree = BeautifulSoup(byline, "lxml")
+                    byline = tree.get_text()
+                    # print(byline)
+                    items['byline'] = byline
+            else :
                 tree = BeautifulSoup(byline, "lxml")
                 byline = tree.get_text()
                 # print(byline)
                 items['byline'] = byline
-        else :
-            tree = BeautifulSoup(byline, "lxml")
-            byline = tree.get_text()
-            # print(byline)
-            items['byline'] = byline
-        # print(items)
-        yield items
+            # print(items)
+            yield items
 
 
 
@@ -53,8 +53,4 @@ class QuoteSpider(scrapy.Spider):
 
 
 
-# HEADLINES
-# DATE AND TIME
-# ARTICLE LINK
-# NEWS LABEL
-# ARTICLE LINK
+# All done just host the csv file up and be done with it
